71b83bb92bfcec277cb0b2b548e55fe0fc57000a
Gerasimov Jegor <herasimau.jahor@gmail.com>
2020-09-15 21:30:54 +0300

    t2 fix csv

--- src/main/java/yandex/utils/Helper.java
+++ src/main/java/yandex/utils/Helper.java
@@ -7,13 +7,14 @@ import java.util.List;
 
 public class Helper {
 
-    private static final String CATEGORIES_FILE_PATH = PropertyManager.getProperty("resources.path") + "categories.csv";
+    private static String CATEGORIES_FILE_PATH;
 
     public static String toMultiOS(String path) {
         return path.replace("/", File.separator);
     }
 
     public static void writeCategoriesToFile(List<String> categories) {
+        CATEGORIES_FILE_PATH = PropertyManager.getProperty("resources.path") + "categories.csv";
         try (PrintWriter writer = new PrintWriter(new File(CATEGORIES_FILE_PATH))) {
             StringBuilder sb = new StringBuilder();
             for (String category : categories) {
@@ -26,6 +27,7 @@ public class Helper {
     }
 
     public static boolean categoriesFileIsExists() {
+        CATEGORIES_FILE_PATH = PropertyManager.getProperty("resources.path") + "categories.csv";
         File file = new File(CATEGORIES_FILE_PATH);
         return file.exists() && !file.isDirectory();
     }
